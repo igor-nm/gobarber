c948cced714dacd42860e92d2d206871fa4455b1
Igor Martucelli <martucelli.igor@gmail.com>
2019-10-22 00:34:26 -0300

    feature: Make a database connection using postgres with sequelize. Also do a sample from migration and models showing how to put data on database by request.
    It was done, user's migration and model.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: { es6: true, node: true },
-    extends: [ 'prettier', 'airbnb-base' ],
+    extends: [ 'airbnb-base', 'prettier' ],
     plugins: [ 'prettier' ],
     globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
     parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
 
+import User from "./app/models/User";
+
 const routes = Router();
 
-routes.get("/", (request, response) => response.json({ status: true }));
+routes.get("/", async (request, response) => {
+    const user = await User.create({
+        provider: true,
+        name: "Igor Martucelli",
+        email: "martucelli.igor@gmail.com",
+        password_hash: "dfinapdijnpiaOPNIOPAFGJN414E3Q1678",
+    });
+
+    return response.json(user);
+});
 
 export default routes;
